fefdb9f6e61f355ea144ef8fbf600a47e13cd399
cupofmilk7 <cupofmilk80@gmail.com>
2024-10-16 12:24:02 +0300

    Added the ability to select the type of sorting (ascending, descending)

--- sort.cpp
+++ sort.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 
+#include "checkData.h"
+
 void swap(int* num1, int* num2) {
     int temp = *num1;
     *num1 = *num2;
@@ -8,13 +10,33 @@ void swap(int* num1, int* num2) {
 }
 
 void sort(std::vector<int>& array) {
-    int index = 1;
-    while (index < array.size()) {
-        if(index == 0 || array[index - 1] <= array[index]) {
-            index++;
-            continue;
-        }
-        swap(&array[index - 1], &array[index]);
-        index--;
+    std::cout << "1. Ascending, 2. Descending" << std::endl;
+    int choice = checkInt();
+    while (choice < 1 || choice > 2) {
+        std::cout << "Choose an option from 1 to 2" << std::endl;
+        choice = checkInt();
     }
+    if (choice == 1) {
+        int index = 1;
+        while (index < array.size()) {
+            if(index == 0 || array[index - 1] <= array[index]) {
+                index++;
+                continue;
+            }
+            swap(&array[index - 1], &array[index]);
+            index--;
+        }
+    }
+    else {
+        int index = 1;
+        while (index < array.size()) {
+            if(index == 0 || array[index - 1] >= array[index]) {
+                index++;
+                continue;
+            }
+            swap(&array[index - 1], &array[index]);
+            index--;
+        }
+    }
+
 }
